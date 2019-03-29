c059b2da0b1d9fb6d74d96135c25327532eed493
shilpashreevs <48344640+shilpashreevs@users.noreply.github.com>
2019-03-29 17:21:15 +0530

    Add files via upload

---
+++ stringprogram.java
@@ -0,0 +1,22 @@
+public class SortingArray
+{
+    public static void main(String []args)
+    {
+        String str = "fguecbdavwyxzhijklmnopqrst";
+	String[] strArr = {"geeksforgeeks", "is", "the", "best", "place", "for", "learning"};
+	String output = "";
+
+	for(int i = 0;i < str.length();i++)
+	{
+	    for(int j = 0;j < strArr.length;j++)
+	    {
+	     	if(str.charAt(i) == strArr[j].charAt(0))
+		{
+		    output = output + " " + strArr[j];
+		}
+		
+	    }
+	}
+	System.out.println(output);
+    }
+}
