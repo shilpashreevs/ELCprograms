5c594558acee696941a81e1a0f911a0bb931da7a
shilpashreevs <48344640+shilpashreevs@users.noreply.github.com>
2019-04-02 15:55:20 +0530

    Update nearestgreaterelement.java

--- nearestgreaterelement.java
+++ nearestgreaterelement.java
@@ -1,34 +1,28 @@
-import java.util.Scanner;
-public class NextGreater{
-public static void greater(int arr[])
-{
-int next;
-       for (int i=0; i<arr.length; i++)
-       {
-           next = -1;
-           for (int j = i+1; j<arr.length; j++)
-           {
-               if (arr[i] < arr[j])
-               {
-                   next = arr[j];
-                   break;
-               }
-           }
-           System.out.println(arr[i]+" = "+next);
-       }
-}
-public static void main(String[] args) {
-Scanner sc=new Scanner (System.in);
-              System.out.println("enter the array size:");
-              int size=sc.nextInt();
-             
-              System.out.println("enter the array :");
-               int[] arr=new int[size];
-               for(int i=0;i<size;i++)
-               {
-                   arr[i]=sc.nextInt();
-               }
-               System.out.println("Next greater elements are :");
-greater(arr);
-}
-}
+ public class Main 
+{  
+  
+ public static void printNGE(int arr[], int n) 
+    { 
+        int next, i, j; 
+        for (i=0; i<n; i++) 
+        { 
+            next = -1; 
+            for (j = i+1; j<n; j++) 
+            { 
+                if (arr[i] < arr[j]) 
+                { 
+                    next = arr[j]; 
+                    break; 
+                } 
+            } 
+            System.out.println(arr[i]+" -- "+next); 
+        } 
+    } 
+       
+    public static void main(String args[]) 
+    { 
+        int arr[]= {5, 6, 3, 49}; 
+        int n = arr.length; 
+        printNGE(arr, n); 
+    } 
+} 
